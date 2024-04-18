e9b8f4051d2113057777a06c61181608f2bea38c
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-04-17 22:00:21 -0500

    cambios visuales y se agrego un formulario reactivo mas un boton

--- src/app/products/pages/product-page/product-page.component.ts
+++ src/app/products/pages/product-page/product-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './product-page.component.html',
@@ -6,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class ProductPageComponent {
 
+  private _formbuilder = inject(FormBuilder);
+
+  public color: string = 'red';
+
+  public formulario: FormGroup = this._formbuilder.group({
+    name: ['', [Validators.required, Validators.minLength(6), Validators.email]]
+  });
+  //un tipo de injeccion
+  // constructor(private _formbuilder: FormBuilder) { }
+  cambiarColor(): void {
+    this.color = '#xxxxxx'.replace(/x/g, y => (Math.random() * 16 | 0).toString(16));
+  }
 }
