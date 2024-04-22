280cd05112b3594cff23be89fb10136b82e2be5c
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-04-22 17:57:56 -0500

    llenado de la pantalla y llamado al api

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { UsersServiceService } from '../../services/users-service.service';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './user-info-page.component.html',
   styleUrl: './user-info-page.component.css'
 })
-export class UserInfoPageComponent {
+export class UserInfoPageComponent implements OnInit {
 
+  private _userService = inject(UsersServiceService);
+
+  public idUsuario = signal(1);
+  public usuarioActual = signal<User | undefined>(undefined);
+  public usuarioEncontrado = signal(true);
+
+  ngOnInit(): void {
+    this.cargarUsuario(this.idUsuario());
+  }
+
+  cargarUsuario(id: number) {
+    if (id <= 0) { return }
+
+    this.usuarioActual.set(undefined);
+    //establecemos el valor del id
+    this.idUsuario.set(id);
+
+    this._userService.obtenerUsuarioPorId(id)
+      .subscribe(usuario => {
+        this.usuarioActual.set(usuario);
+      })
+
+  }
 }
