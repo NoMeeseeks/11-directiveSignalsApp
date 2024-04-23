1bfce996d8cbbe25be59628a9d805f3d259ef63a
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-04-22 19:39:34 -0500

    la mutacion ya no esta disponible en angular 17 , se quedo el update

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { User } from '../../interfaces/user-request.interface';
 
 @Component({
@@ -14,8 +14,36 @@ export class PropertiesPageComponent {
     last_name: 'Blut',
     avatar: 'https://regres.in/img/faces/1-image.jpg',
   });
+  public nombreCompleto = computed(() => { `${this.usuario().first_name} ${this.usuario().last_name}` })
 
-  onFieldUpdated(campo: string, valor: string) {
-    console.log(campo, valor);
+  onFieldUpdated(campo: keyof User, valor: string) {
+    // this.usuario.set({
+    //   ...this.usuario(), [campo]: valor
+    // })
+    this.usuario.update(valorActual => {
+
+      switch (campo) {
+        case 'email':
+          valorActual.email = valor;
+          break;
+        case 'avatar':
+          valorActual.avatar = valor;
+          break;
+        case 'first_name':
+          valorActual.first_name = valor;
+          break;
+        case 'last_name':
+          valorActual.last_name = valor;
+          break;
+        case 'id':
+          valorActual.id = Number(valor);
+          break;
+
+      }
+
+
+      return valorActual
+    })
+
   }
 }
